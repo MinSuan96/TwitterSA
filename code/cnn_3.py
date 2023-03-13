e6e425a4411c491d7327794c32bda6433d5f070f
Minsuan Teh <minsuan1104@live.com>
2023-03-13 01:38:41 +0000

    refactor test and train function in cnn_3

--- code/cnn_3.py
+++ code/cnn_3.py
@@ -18,8 +18,7 @@ TRAIN_PROCESSED_FILE = '../twitter_data/3-sentiment-processed-train.csv'
 TEST_PROCESSED_FILE = '../twitter_data/3-sentiment-processed-X-test.csv'
 TEST_LABEL_FILE = '../twitter_data/3-sentiment-processed-y-test.csv'
 GLOVE_FILE = '../dataset/glove-seeds.txt'
-MODEL_FILE = './models/4cnn-08-0.020-0.283.hdf5'
-train = True
+MODEL_FILE = './models/cnn-08-0.331-0.706.hdf5'
 dim = 200
 
 
@@ -93,67 +92,77 @@ def process_tweets(csv_file, test_file=True):
     print('\n')
     return tweets, np.array(labels)
 
+def train(vocab, vocab_size, max_length, layers, filters, kernel_size, report_file, train_and_test=False):
+    tweets, labels = process_tweets(TRAIN_PROCESSED_FILE, test_file=False)
+    glove_vectors = get_glove_vectors(vocab)
+    # Create and embedding matrix
+    embedding_matrix = np.random.randn(vocab_size + 1, dim) * 0.01
+    # Seed it with GloVe vectors
+    for word, i in vocab.items():
+        glove_vector = glove_vectors.get(word)
+        if glove_vector is not None:
+            embedding_matrix[i] = glove_vector
+    tweets = pad_sequences(tweets, maxlen=max_length, padding='post')
+    shuffled_indices = np.random.permutation(tweets.shape[0])
+    tweets = tweets[shuffled_indices]
+    labels = labels[shuffled_indices]
+    model = Sequential()
+    model.add(Embedding(vocab_size + 1, dim, weights=[embedding_matrix], input_length=max_length))
+    model.add(Dropout(0.4))
+    if layers > 3:
+        for i in range(layers - 3):
+            model.add(Conv1D(filters, kernel_size, padding='valid', activation='relu', strides=1))
+    if layers >= 3:
+        model.add(Conv1D(300, kernel_size, padding='valid', activation='relu', strides=1))
+    if layers >= 2:
+        model.add(Conv1D(150, kernel_size, padding='valid', activation='relu', strides=1))
+    model.add(Conv1D(75, kernel_size, padding='valid', activation='relu', strides=1))
+    model.add(Flatten())
+    model.add(Dense(600))
+    model.add(Dropout(0.5))
+    model.add(Activation('relu'))
+    # Change output layer to have 3 nodes and softmax activation
+    model.add(Dense(3)) 
+    model.add(Activation('softmax'))
+    # Change loss function to categorical_crossentropy
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    filepath = "./models/cnn-{epoch:02d}-{loss:0.3f}-{val_loss:0.3f}.hdf5"
+    checkpoint = ModelCheckpoint(filepath, monitor="loss", verbose=1, save_best_only=True, mode='min')
+    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=2, min_lr=0.000001)
+    model.fit(tweets, labels, batch_size=128, epochs=8, validation_split=0.1, shuffle=True, callbacks=[checkpoint, reduce_lr])
+    if train_and_test:
+        test(layers, kernel_size, batch_size, max_length, report_file, model=model)
+        
+def test(layers, kernel_size, batch_size, max_length, report_file, model=None):
+    if model == None:
+        model = load_model(MODEL_FILE)
+    print(model.summary())
+    test_tweets, _ = process_tweets(TEST_PROCESSED_FILE, test_file=True)
+    test_tweets = pad_sequences(test_tweets, maxlen=max_length, padding='post')
+    predictions = model.predict(test_tweets, batch_size=batch_size, verbose=1)
+    results = np.argmax(predictions, axis=1) - 1 # Convert back to original labels (-1, 0, 1)
+    id_results = zip(map(str, range(len(test_tweets))), results)
+    utils.save_results_to_csv(id_results, './predictions/3-sentiments-predictions-{}cnn-{}kernel-{}mlength.csv'.format(layers, kernel_size, max_length))
+    test_label = utils.file_number_to_list(TEST_LABEL_FILE)
+    report = classification_report(test_label, results, target_names=['negative', 'neutral', 'positive'], output_dict=True)
+    print(classification_report(test_label, results, target_names=['negative', 'neutral', 'positive'], output_dict=False))
+    df_report = pd.DataFrame(report).transpose()
+    df_report.to_csv(report_file)
 
 if __name__ == '__main__':
     np.random.seed(1337)
     vocab_size = 90000
-    batch_size = 500
+    batch_size = 128
     max_length = 10
     filters = 600
-    kernel_size = 3
-    layers = 3
+    kernel_size = [5, 7]
+    layers = [2, 1]
     vocab = utils.top_n_words(FREQ_DIST_FILE, vocab_size, shift=1)
-    REPORT_FILE = './reports/3-sentiments-{}cnn-{}kernel.csv'.format(layers, kernel_size)
-
-    if train:  
-        tweets, labels = process_tweets(TRAIN_PROCESSED_FILE, test_file=False)
-        glove_vectors = get_glove_vectors(vocab)
-        # Create and embedding matrix
-        embedding_matrix = np.random.randn(vocab_size + 1, dim) * 0.01
-        # Seed it with GloVe vectors
-        for word, i in vocab.items():
-            glove_vector = glove_vectors.get(word)
-            if glove_vector is not None:
-                embedding_matrix[i] = glove_vector
-        tweets = pad_sequences(tweets, maxlen=max_length, padding='post')
-        shuffled_indices = np.random.permutation(tweets.shape[0])
-        tweets = tweets[shuffled_indices]
-        labels = labels[shuffled_indices]
-        model = Sequential()
-        model.add(Embedding(vocab_size + 1, dim, weights=[embedding_matrix], input_length=max_length))
-        model.add(Dropout(0.4))
-        if layers > 3:
-            for i in range(layers - 3):
-                model.add(Conv1D(filters, kernel_size, padding='valid', activation='relu', strides=1))
-        if layers >= 3:
-            model.add(Conv1D(300, kernel_size, padding='valid', activation='relu', strides=1))
-        if layers >= 2:
-            model.add(Conv1D(150, kernel_size, padding='valid', activation='relu', strides=1))
-        model.add(Conv1D(75, kernel_size, padding='valid', activation='relu', strides=1))
-        model.add(Flatten())
-        model.add(Dense(600))
-        model.add(Dropout(0.5))
-        model.add(Activation('relu'))
-        # Change output layer to have 3 nodes and softmax activation
-        model.add(Dense(3)) 
-        model.add(Activation('softmax'))
-        # Change loss function to categorical_crossentropy
-        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-        filepath = "./models/cnn-{epoch:02d}-{loss:0.3f}-{val_loss:0.3f}.hdf5"
-        checkpoint = ModelCheckpoint(filepath, monitor="loss", verbose=1, save_best_only=True, mode='min')
-        reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=2, min_lr=0.000001)
-        model.fit(tweets, labels, batch_size=128, epochs=8, validation_split=0.1, shuffle=True, callbacks=[checkpoint, reduce_lr])
+    report_file = './reports/3-sentiments-report-{}cnn-{}kernel-{}mlength.csv'.format(layers, kernel_size, max_length)
+    training = True
+    train_and_test = True
+    if training:  
+        for i in range(2):
+            train(vocab, vocab_size, max_length, layers[i], filters, kernel_size[i], report_file, train_and_test)
     else:
-        model = load_model(MODEL_FILE)
-        print(model.summary())
-        test_tweets, _ = process_tweets(TEST_PROCESSED_FILE, test_file=True)
-        test_tweets = pad_sequences(test_tweets, maxlen=max_length, padding='post')
-        predictions = model.predict(test_tweets, batch_size=128, verbose=1)
-        results = np.argmax(predictions, axis=1) - 1 # Convert back to original labels (-1, 0, 1)
-        id_results = zip(map(str, range(len(test_tweets))), results)
-        utils.save_results_to_csv(id_results, 'cnn.csv')
-        test_label = utils.file_number_to_list(TEST_LABEL_FILE)
-        report = classification_report(test_label, results, target_names=['negative', 'neutral', 'positive'], output_dict=True)
-        print(classification_report(test_label, results, target_names=['negative', 'neutral', 'positive'], output_dict=False))
-        df_report = pd.DataFrame(report).transpose()
-        df_report.to_csv(REPORT_FILE)
+        test(layers, kernel_size, batch_size, max_length, report_file)
